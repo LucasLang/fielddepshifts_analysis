ee3714d3ef10c54326731a8e418b41b8b25f6e81
Lucas Lang <lucas.lang@chem.tu-berlin.de>
2025-02-20 09:47:48 +0100

    Small modifications of convergence plot.

--- workflow/scripts/plot_convergence_gridsize.py
+++ workflow/scripts/plot_convergence_gridsize.py
@@ -9,10 +9,11 @@ max = data[:,2]
 firstgrid = 1
 lastgrid = 31
 x = range(firstgrid, lastgrid+1)
-plt.plot(x, mean)
-plt.fill_between(x, max, min, alpha=0.2)
+plt.plot(x, mean, label="mean")
+plt.fill_between(x, max, min, alpha=0.2, label="min to max")
 plt.yscale("log")
 plt.xlim(firstgrid, lastgrid)
 plt.xlabel("Grid")
-plt.ylabel("Error")
+plt.ylabel("Relative deviation")
+plt.legend()
 plt.savefig("convergence_gridsize.png", dpi=300)
